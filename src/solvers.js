fdec5f34bcc85ad37f57938c226954b8772388e5
Sheena Ramirez <sheenaramirez2@gmail.com>
2017-05-25 19:01:13 -0700

    "Add solutions for finding an n-Queens solution and the number of possible n-Queens solutions"

--- src/solvers.js
+++ src/solvers.js
@@ -47,7 +47,30 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({n:n});
+  var solution;
+
+  if (n === 2 || n === 3) {
+    return board.rows();
+  }
+
+  var findSolution = function (row) {
+    if (row === n) {
+      solution = board.rows().map(function(row) {
+        return row.slice();
+      });
+      return;
+    }
+    for (var i = 0; i < n; i++) {
+      board.togglePiece(row, i);
+      if (!board.hasAnyQueensConflicts()) {
+        findSolution(row + 1);
+      }
+      board.togglePiece(row, i);
+    }
+  };
+
+  findSolution(0);
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -55,8 +78,24 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({n:n});
 
+  var findSolution = function (row) {
+    if (row === n) {
+      solutionCount++;
+      return;
+    }
+    for (var i = 0; i < n; i++) {
+      board.togglePiece(row, i);
+      if (!board.hasAnyQueensConflicts()) {
+        findSolution(row + 1);
+      }
+      board.togglePiece(row, i);
+    }
+  };
+
+  findSolution(0);
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
